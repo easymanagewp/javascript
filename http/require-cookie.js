334091bbeaa23b543959204522e3ce186d4553f3
roy <roy@labsoft.cn>
2015-11-10 14:20:56 +0800

    添加cookie工具类

---
+++ http/require-cookie.js
@@ -0,0 +1,36 @@
+define(function() {
+	var getsec = function(str) {
+		var str1 = str.substring(1, str.length) * 1;
+		var str2 = str.substring(0, 1);
+		if (str2 == "s") {
+			return str1 * 1000;
+		} else if (str2 == "h") {
+			return str1 * 60 * 60 * 1000;
+		} else if (str2 == "d") {
+			return str1 * 24 * 60 * 60 * 1000;
+		}
+	}
+
+	return {
+		'get': function(name) {
+			var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+			if (arr = document.cookie.match(reg))
+				return unescape(arr[2]);
+			else
+				return null;
+		},
+		'del' : function(name){
+			var exp = new Date();
+			exp.setTime(exp.getTime() - 1);
+			var cval = getCookie(name);
+			if (cval != null)
+				document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+		},
+		'set' : function(name, value, time){
+			var strsec = getsec(time);
+			var exp = new Date();
+			exp.setTime(exp.getTime() + strsec * 1);
+			document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+		}
+	};
+});
