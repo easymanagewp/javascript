ea5e36b27efe77d5ced360ae909d0aeb2154f628
roy <roy@labsoft.cn>
2015-11-10 14:24:07 +0800

    cookie

--- http/require-cookie.js
+++ http/require-cookie.js
@@ -11,26 +11,30 @@ define(function() {
 		}
 	}
 
-	return {
-		'get': function(name) {
+	var __get = function(name) {
 			var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
 			if (arr = document.cookie.match(reg))
 				return unescape(arr[2]);
 			else
 				return null;
 		},
-		'del' : function(name){
+		__del = function(name){
 			var exp = new Date();
 			exp.setTime(exp.getTime() - 1);
-			var cval = getCookie(name);
+			var cval = __get(name);
 			if (cval != null)
 				document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
 		},
-		'set' : function(name, value, time){
+		__set = function(name, value, time){
 			var strsec = getsec(time);
 			var exp = new Date();
 			exp.setTime(exp.getTime() + strsec * 1);
 			document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
 		}
+
+	return {
+		'get': __get,
+		'del' : __del,
+		'set' : __set
 	};
 });
